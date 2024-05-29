01edc0feeee9a9b86beb6e71cfecb2b300ae9f8f
kristakutsar <krista.kutsar00@gmail.com>
2024-05-29 23:30:02 +0300

    up

--- kontakt.php
+++ kontakt.php
@@ -18,7 +18,7 @@
     <nav class="navbar navbar-expand-lg bg-body-light">
         <div class="container-fluid">
             <img src="ikoon.jpg" width="40" height="40" alt="">
-          <a class="navbar-brand fs-6"  href="index.php">Avaleht</a>
+          <a class="navbar-brand fs-6"  href="index.phps">Avaleht</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
